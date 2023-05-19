ca2a388903e95a4dcb1142c1be1523d4250ef00a
Surain <surain.wira@gmail.com>
2023-05-19 21:00:31 +0800

    ADDED RANDOM PRIZE MONEY FOR SUCCESSFUL JACKPOTS

--- src/Machine.js
+++ src/Machine.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 
 class Machine extends React.Component {
+
     randomAlphabet() {
         const alphabets = ['X', 'Y', 'Z'];
         const randomNumber = Math.floor(Math.random() * 3);
         return alphabets[randomNumber];
+    }
+    randomPrize() {
+        const randomNumber = Math.floor(Math.random() * 3);
+        const prizeCategories = ['10,000', '100,000', '100,000,000'];
+        return prizeCategories[randomNumber]
     }
     render() {
         //assigning random alphabets on s1,s2,s3
@@ -18,6 +24,7 @@ class Machine extends React.Component {
             return <div className='Machine'>
                 <p>{s1} {s2} {s3}</p>
                 <p className='Machine-win'><b>SUCCESS</b></p>
+                <p>YOU WON RM {this.randomPrize()}! CONGRATS!</p>
             </div>
         }
         //this div is displayed when s1,s2,s3 doesnt have same value
