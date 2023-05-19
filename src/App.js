b15f1ab731f26f06a5fe7932f68f43fff2ff7642
Surain <surain.wira@gmail.com>
2023-05-19 21:05:51 +0800

    UPDATED H1 and H3 of homepage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <h1> THIS IS A JACKPOT MACHINE, CLICK REFRESH TO SPIN THE WHEELS</h1>
+        <h1> SIMPLEST JACKPOT MACHINE</h1>
+        <h3>REFRESH TO SPIN THE WHEELS</h3>
         {/* This component showcase display 
         dynamically without props */}
         <Machine />
